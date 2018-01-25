063b0c1aec8cd2430989e3c740e9ad852824c6bf
Karl Cronburg <karl.cronburg@gmail.com>
2018-01-25 16:28:30 -0500

    rearrange header info into application, and change interface a bit

--- src/main-mmap.c
+++ src/main-mmap.c
@@ -2,6 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+
+// ----------------------------------------------------------------------------
+// The following might end up in its own header file eventually, but for now
+// only the application can really know how to set the right types and system
+// calls.
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+typedef int Int;
+typedef size_t SizeT;
+typedef unsigned long int Addr;
+typedef unsigned char U8; //UChar;
+
+void  shadow_free(void* addr) { free(addr); }
+void *shadow_malloc(size_t size) { return malloc(size); }
+void *shadow_calloc(size_t nmemb, size_t size) { return calloc(nmemb, size); }
+void  shadow_memcpy(void* dst, void* src, size_t size) { memcpy(dst,src,size); }
+void  shadow_out_of_memory() {
+  printf("ERROR: Ran out of memory while allocating shadow memory.\n");
+  exit(1);
+}
+// ----------------------------------------------------------------------------
+
 #include "shadow-mmap.h"
 
 #define print(u8) printf("tmp = 0x%x\n", u8)
@@ -12,7 +41,7 @@ int main(int argc, char *argv[]) {
   printf("Entering main()\n");
 
   ShadowMap* my_sm = malloc(sizeof(ShadowMap));
-  shadow_initialize_map(my_sm);
+  shadow_initialize_with_mmap(my_sm);
   U8 tmp;
   
   shadow_set_bits(my_sm, 0, (U8)0xab);
--- src/shadow-mmap.h
+++ src/shadow-mmap.h
@@ -1,34 +1,12 @@
 #ifndef shadow_mmap_h__
 #define shadow_mmap_h__
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <stddef.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-
-typedef int Int;
-typedef size_t SizeT;
-typedef unsigned long int Addr;
-typedef unsigned char U8; //UChar;
-
-void  shadow_free(void* addr) { free(addr); }
-void *shadow_malloc(size_t size) { return malloc(size); }
-void *shadow_calloc(size_t nmemb, size_t size) { return calloc(nmemb, size); }
-void  shadow_memcpy(void* dst, void* src, size_t size) { memcpy(dst,src,size); }
-void  shadow_out_of_memory() {
-  printf("ERROR: Ran out of memory while allocating shadow memory.\n");
-  exit(1);
-}
 
 // >>> hex(2**35 - 1)
 //#define LOW_HALF_BITS 0x00000007ffffffff
 
 // >>> hex(2**18 - 1)
 #define LOW_BITS 0x3ffff
-typedef struct { U8 bits[262144]; } Low; // 2**18 bits (bit for bit shadow map)
+typedef struct { U8 bits[262144]; } Low; // 2**18 bytes (bit for bit shadow map)
 
 // >>> hex(2**(17+18) - 1 - (2**18 - 1))
 #define MIDDLE_BITS 0x7fffc0000
@@ -94,10 +72,8 @@ Low* get_Low_for_writing(ShadowMap *PM, Addr a) {
   MiddleLowPtr* ml = &(BITS(PM)[a >> 35]);
   if (ml->l == DMAP(PM)) {
     ml->m = (Middle*)shadow_malloc(sizeof(Middle));
-    printf("(ml->m, ml) = (%p, %p)\n", ml->m, ml);
     memcpy(ml->m, PM->distinguished_middle, sizeof(Middle));
   }
-  printf("(ml->m, ml) = (%p, %p)\n", ml->m, ml);
   Low** low = &(ml->m->bits[(a & MIDDLE_BITS) >> 18]);
   if (*low == DMAP(PM))
     *low = copy_for_writing(*low);
@@ -134,12 +110,17 @@ void shadow_get_bit(ShadowMap *PM, Addr a, U8 offset, U8* bit) {
   *bit = (low->bits[a & LOW_BITS] & (1 << offset)) >> offset;
 }
 
+#define HIGH_SIZE 4294967296
+#define LOW_SIZE (sizeof(Low))
+#define MID_SIZE (sizeof(Middle))
+#define SHADOW_INIT_SIZE (HIGH_SIZE + LOW_SIZE + MID_SIZE)
+
 INLINE
 void shadow_initialize_map(ShadowMap* PM) {
   SizeT i;
 
   // mmap allocate virtual space of the primary map.
-  PM->map = mmap(NULL, 4294967296, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+  //PM->map = mmap(NULL, HIGH_SIZE, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
   if (PM->map == MAP_FAILED) {
     perror("Failed to allocate primary map.");
     return;
@@ -147,24 +128,40 @@ void shadow_initialize_map(ShadowMap* PM) {
 
   // Allocate the distinguished low map and have all entries of the primary map
   // point to it by memcpy-ing the contents of an array pointing to it.
-  DMAP(PM) = (Low*) shadow_calloc(1, sizeof(Low));
+  //DMAP(PM) = (Low*) shadow_calloc(1, LOW_SIZE);
   Low* src[4096];
   for (i = 0; i < 4096; i++) {
     src[i] = DMAP(PM);
   }
   // units(i) = bytes, units(memcpy) = bytes
-  for (i = 0; i < 4294967296; i += 4096 * 8) {
-    printf("(& BITS(PM)) + i = %p\n", (void*)(& BITS(PM)) + i);
+  for (i = 0; i < HIGH_SIZE; i += 4096 * 8) {
     memcpy((void*)(& BITS(PM)) + i, (void*)src, 4096 * 8);
   }
-  printf("&(BITS(PM)) = [%p, %p]\n", &(BITS(PM)), (void*)&(BITS(PM)) + 4294967296);
 
   // Set the distinguished middle map to have all entries pointing to the
   // (single) distinguished low map.
-  PM->distinguished_middle = (Middle*) shadow_malloc(sizeof(Middle));
+  //PM->distinguished_middle = (Middle*) shadow_malloc(MID_SIZE);
   for (i = 0; i < 131072; i++) {
     PM->distinguished_middle->bits[i] = DMAP(PM);
   }
+}
+
+INLINE
+void shadow_initialize_with_memory(void* mem, ShadowMap* PM) {
+  // TODO: assert good alignment
+  PM->map = mem;
+  DMAP(PM) = (Low*)(((char*)mem) + HIGH_SIZE);
+  memset(DMAP(PM), 0, LOW_SIZE);
+  PM->distinguished_middle = (Middle*)(((char*)mem) + HIGH_SIZE + LOW_SIZE);
+  shadow_initialize_map(PM);
+}
+
+INLINE
+void shadow_initialize_with_mmap(ShadowMap* PM) {
+  PM->map = mmap(NULL, HIGH_SIZE, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+  DMAP(PM) = (Low*) shadow_calloc(1, LOW_SIZE);
+  PM->distinguished_middle = (Middle*) shadow_malloc(MID_SIZE);
+  shadow_initialize_map(PM);
 }
 
 INLINE
